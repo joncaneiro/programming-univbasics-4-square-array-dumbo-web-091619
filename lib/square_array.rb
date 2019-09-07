a88605b587d9bf9eccb628f5dbc80c2c575bdf38
Jon Caneiro <joncaneiro@gmail.com>
2019-09-07 22:23:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,11 +2,9 @@ def square_array(array)
   
   index = 0
   
-  while array[index] do
-    
-  num = array[index]
-    
-  puts num ** 1
+ while array.length > index do
+   
+  puts array[index] ** 1
   
   index += 1 
   
